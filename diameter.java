9bda7ea7064be1251a14a4a667b08d1570530896
Prabhakar <puligaddaprabhakar@gmail.com>
2025-03-16 21:16:03 +0530

    20-02-2025/Programs

---
+++ diameter.java
@@ -0,0 +1,14 @@
+//1.Wajp to print diameter of a circle
+
+class Diameter
+{
+public static void main(String args[])
+{
+ double r=20.0;
+ double diameter=2*r;
+ System.out.println(diameter);
+
+}
+
+
+}

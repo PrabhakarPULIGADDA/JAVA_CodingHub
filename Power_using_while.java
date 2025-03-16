fb22ef81c694b913e3b3c08fd635d22838191142
Prabhakar <puligaddaprabhakar@gmail.com>
2025-03-16 21:41:58 +0530

    08-03-2025/Programs

---
+++ Power_using_while.java
@@ -0,0 +1,13 @@
+class Power_using_while
+{
+    public static void main(String args[])
+   {
+	int a=2,b=4,power=1;
+		while(b!=0)
+		{
+		power*=a;
+		b--;
+		}
+		System.out.println(power);
+    }
+}
